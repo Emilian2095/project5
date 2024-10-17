af9a10db733694cc95612a2c8a3006cc8ab48602
Emilian2095 <valentyneemilian@yahoo.com>
2024-10-17 14:10:46 +0200

    Phone validator

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const input = document.getElementById("input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const results = document.getElementById("results-div");
+
+checkBtn.addEventListener("click", ()=>{
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s-]?)\d{3}([\s-]?)\d{4}$/
+
+  if(!input.value){
+ results.innerText = "Please provide a phone number"
+ alert("Please provide a phone number");
+
+  }else if(regex.test(input.value)){
+    results.innerText = `Valid US number: ${input.value}`;
+  }else {
+    results.innerText = `Invalid US number: ${input.value}`;
+  }
+});
+
+clearBtn.addEventListener("click", () =>{
+results.innerText = "";
+input.value = ""
+} )
+
+
